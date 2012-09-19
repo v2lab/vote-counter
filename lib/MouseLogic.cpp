80015020a06047edf58f67d2d7fae321e2f93455
Artem Baguinski <artm@v2.nl>
2012-09-19 11:55:03 +0200

    [qartm] MouseLogic
    detects clicks and rect selections in QGraphicsScene

---
+++ lib/MouseLogic.cpp
@@ -0,0 +1,58 @@
+#include "MouseLogic.hpp"
+
+#include <QObject>
+#include <QGraphicsScene>
+
+MouseLogic::MouseLogic(QGraphicsScene *parent) :
+    QObject(parent),
+    m_state(IDLE)
+{
+    parent->installEventFilter(this);
+}
+
+bool MouseLogic::eventFilter(QObject *, QEvent *event)
+{
+    QGraphicsSceneMouseEvent * gsm_event = dynamic_cast<QGraphicsSceneMouseEvent *>(event);
+
+    switch( event->type() ) {
+    case QEvent::GraphicsSceneMousePress:
+        Q_ASSERT(gsm_event);
+        m_state = PRESSED;
+        m_firstPoint = gsm_event->scenePos();
+        return true; // event's handled
+    case QEvent::GraphicsSceneMouseRelease:
+        Q_ASSERT(gsm_event);
+        switch(m_state) {
+        case PRESSED:
+            emit pointClicked( gsm_event->scenePos(), gsm_event->button(), gsm_event->modifiers() );
+            m_state = IDLE;
+            return true;
+        case SELECTING_RECT:
+            m_state = IDLE;
+            emit rectSelected( makeRect( m_firstPoint, gsm_event->scenePos() ), gsm_event->button(), gsm_event->modifiers());
+            return true;
+        }
+        break;
+    case QEvent::GraphicsSceneMouseMove:
+        Q_ASSERT(gsm_event);
+        switch(m_state) {
+        case PRESSED:
+            m_state = SELECTING_RECT;
+        case SELECTING_RECT:
+            emit rectUpdated( makeRect( m_firstPoint, gsm_event->scenePos() ), gsm_event->button(), gsm_event->modifiers());
+            return true;
+        }
+        break;
+    default:
+        break;
+    }
+
+    return false;
+}
+
+QRectF MouseLogic::makeRect(const QPointF &p1, const QPointF &p2)
+{
+    QPointF tl( std::min(p1.x(), p2.x()), std::min(p1.y(),p2.y())),
+            br( std::max(p1.x(), p2.x()), std::max(p1.y(),p2.y()));
+    return QRectF( tl, br );
+}
---
+++ lib/MouseLogic.hpp
@@ -0,0 +1,35 @@
+#ifndef MOUSELOGIC_HPP
+#define MOUSELOGIC_HPP
+
+#include <QtCore>
+#include <QtGui>
+
+class MouseLogic : public QObject
+{
+    Q_OBJECT
+public:
+    enum State {
+        IDLE,
+        PRESSED,
+        SELECTING_RECT
+    };
+
+    explicit MouseLogic(QGraphicsScene *parent);
+    State state() const { return m_state; }
+
+signals:
+    void pointClicked(QPointF point, Qt::MouseButton button, Qt::KeyboardModifiers mods);
+    void rectUpdated(QRectF rect, Qt::MouseButton button, Qt::KeyboardModifiers mods);
+    void rectSelected(QRectF rect, Qt::MouseButton button, Qt::KeyboardModifiers mods);
+
+public slots:
+
+protected:
+    virtual bool eventFilter(QObject * target, QEvent * event);
+    static QRectF makeRect(const QPointF& p1, const QPointF& p2);
+
+    State m_state;
+    QPointF m_firstPoint;
+};
+
+#endif // MOUSELOGIC_HPP
